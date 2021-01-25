f3ff3e4edb79c435faee367a277625f1793a2bee
James <james.wingar@gmail.com>
2021-01-25 00:32:29 +0000

    Cleaned up and prepared for pygame classes

--- main.py
+++ main.py
@@ -1,87 +1,62 @@
 import time
-import copy
-
-import recursive_sort
-import iterative_sort
+from sort import recursive_sort
+from sort import iterative_sort
 import utility
-
-
-# print(createList(100))
+import gui
 
 numbers = utility.createRandList(100)  # [7, 3, 9, 5, 6, 0, 1, 2, 4, 8]
 
 # RUNNING RECURSIVE ALGORITHMS
-lst = copy.deepcopy(numbers)
-print(lst)
-
+lst = numbers[:]
 tic = time.perf_counter()
-print(recursive_sort.mergeSort(lst))
+recursive_sort.mergeSort(lst)
 toc = time.perf_counter()
 print(f"mergeSort time: {toc - tic:0.7f}")
 
-
-lst = copy.deepcopy(numbers)
-print(lst)
-
+lst = numbers[:]
 tic = time.perf_counter()
-print(recursive_sort.bubbleSort(lst, len(lst)))
+recursive_sort.bubbleSort(lst, len(lst))
 toc = time.perf_counter()
 print(f"bubbleSort time: {toc - tic:0.7f}")
 
-
-lst = copy.deepcopy(numbers)
-print(lst)
-
+lst = numbers[:]
 tic = time.perf_counter()
-print(recursive_sort.quickSort(lst, 0, len(lst) - 1))
+recursive_sort.quickSort(lst, 0, len(lst) - 1)
 toc = time.perf_counter()
 print(f"quickSort time: {toc - tic:0.7f}")
 
-
-lst = copy.deepcopy(numbers)
-print(lst)
-
+lst = numbers[:]
 tic = time.perf_counter()
-print(recursive_sort.heapSort(lst))
+recursive_sort.heapSort(lst)
 toc = time.perf_counter()
 print(f"heapSort time: {toc - tic:0.7f}")
-
-
-
 
 
 # RUNNING ITERATIVE ALGORITHMS
-lst = copy.deepcopy(numbers)
-print(lst)
-
+lst = numbers[:]
 tic = time.perf_counter()
-print(iterative_sort.mergeSort(numbers))
+iterative_sort.mergeSort(lst)
 toc = time.perf_counter()
 print(f"mergeSort time: {toc - tic:0.7f}")
 
-
-lst = copy.deepcopy(numbers)
-print(lst)
-
+lst = numbers[:]
 tic = time.perf_counter()
-print(iterative_sort.bubbleSort(lst, len(lst)))
+iterative_sort.bubbleSort(lst)
 toc = time.perf_counter()
 print(f"bubbleSort time: {toc - tic:0.7f}")
 
-
-lst = copy.deepcopy(numbers)
-print(lst)
-
+lst = numbers[:]
 tic = time.perf_counter()
-print(iterative_sort.quickSort(lst, 0, len(lst) - 1))
+iterative_sort.quickSort(lst)
 toc = time.perf_counter()
 print(f"quickSort time: {toc - tic:0.7f}")
 
-
-lst = copy.deepcopy(numbers)
-print(lst)
-
+lst = numbers[:]
 tic = time.perf_counter()
-print(iterative_sort.heapSort(lst))
+iterative_sort.heapSort(lst)
 toc = time.perf_counter()
 print(f"heapSort time: {toc - tic:0.7f}")
+
+
+visual = gui.Visual_sorting_algorithms(numbers[:])
+visual.run()
